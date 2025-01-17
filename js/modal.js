a7e1beed0d95dbcb565e75c89e15146a45c49ac5
vinidevel <vini_ide@yahoo.com.br>
2025-01-16 21:51:27 -0300

    Incluir Modals e links das redes sociais

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,55 @@
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var modalLanding = document.getElementById("myModalLanding");
+var modalSistemas = document.getElementById("myModalSistemas");
+var modalEcommerce = document.getElementById("myModalEcommerce");
 
-// Get the <span> element that closes the modal
+
+var btnLandind = document.getElementById("myBtnLanding");
+var btnSistemas = document.getElementById("myBtnSistemas");
+var btnEcommerce = document.getElementById("myBtnEcommerce");
+
+
 var span = document.getElementsByClassName("close")[0];
+var spanSistemas = document.getElementsByClassName("close")[1];
+var spanEcomerce = document.getElementsByClassName("close")[2];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+
+btnLandind.onclick = function() {
+  modalLanding.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+btnSistemas.onclick = function() {
+  modalSistemas.style.display = "block";
+}
+
+btnEcommerce.onclick = function() {
+  modalEcommerce.style.display = "block";
+}
+
+
 span.onclick = function() {
-  modal.style.display = "none";
+  modalLanding.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+spanSistemas.onclick = function() {
+  modalSistemas.style.display = "none";
+}
+
+spanEcomerce.onclick = function() {
+  modalEcommerce.style.display = "none";
+}
+
+
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalLanding) {
+    modalLanding.style.display = "none";
+  }
+
+  if (event.target == modalSistemas) {
+    modalSistemas.style.display = "none";
+  }
+
+  if (event.target == modalEcommerce) {
+    modalEcommerce.style.display = "none";
   }
 }
